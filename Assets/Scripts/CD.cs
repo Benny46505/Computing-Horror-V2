0f96e690172d00867204749056fcefd8870aa721
Benny46505 <69089281+Benny46505@users.noreply.github.com>
2022-10-28 12:29:53 -0700

    CD
    CD

--- Assets/Scripts/CD.cs
+++ Assets/Scripts/CD.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class CD : MonoBehaviour
 {
+    public GameManager gm;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(other);
+            gm.cds += 1;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
 {
 
     TextMeshProUGUI cdcount;
-    int cds;
+    public int cds;
 
     // Start is called before the first frame update
     void Start()
